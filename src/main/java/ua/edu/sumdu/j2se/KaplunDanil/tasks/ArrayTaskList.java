afc37cf1839008ae561194980560eebc332207cc
kardank1 <kaplun.danil.1996@gmail.com>
2022-04-02 14:07:16 +0300

    lab2

--- src/main/java/ua/edu/sumdu/j2se/KaplunDanil/tasks/ArrayTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/KaplunDanil/tasks/ArrayTaskList.java
@@ -1,2 +1,97 @@
-package ua.edu.sumdu.j2se.KaplunDanil.tasks;public class ArrayTaskList {
+package ua.edu.sumdu.j2se.KaplunDanil.tasks;
+
+public class ArrayTaskList {
+    private int num;
+    private Task [] tasks;
+    private int iteratorNow;
+
+    public ArrayTaskList(){
+        num = 1;
+        tasks = new Task[num];
+        iteratorNow = 0;
+    }
+
+    public void add(Task task)
+    {
+        tasks[iteratorNow] = task;
+        iteratorNow++;
+        if(iteratorNow == num)
+        {
+           tasks = sizeIncrease(tasks);
+        }
+
+    }
+
+    public boolean remove(Task task)
+    {
+        boolean rem = false;
+
+        for(int i = 0; i < iteratorNow; i++)
+        {
+            if(task.equals(tasks[i]) && !rem)
+            {
+
+                tasks[i] = null;
+                rem = true;
+            }
+            if(rem)
+            {
+                tasks[i] = tasks[i+1];
+                tasks[i+1] = null;
+            }
+        }
+
+        if(rem)
+        {
+            iteratorNow--;
+        }
+        if(iteratorNow < num - 5)
+        {
+           tasks = sizeDecrease(tasks);
+        }
+
+        return rem;
+    }
+
+    public int size()
+    {
+        return iteratorNow;
+    }
+
+    public Task getTask(int index)
+    {
+        return tasks[index];
+    }
+
+    public Task[] sizeIncrease(Task [] tasks)
+    {
+        num += 5;
+        Task [] tasks1 = new Task[num];
+
+        System.arraycopy(tasks, 0, tasks1, 0, tasks.length);
+        return tasks1;
+    }
+
+    public Task[] sizeDecrease(Task [] tasks)
+    {
+        num -= 5;
+        Task [] tasks1 = new Task[num];
+
+        System.arraycopy(tasks, 0, tasks1, 0, tasks.length);
+        return tasks1;
+    }
+
+    ArrayTaskList incoming(int from, int to)
+    {
+        ArrayTaskList actual = new ArrayTaskList();
+
+        for(Task temp:tasks)
+        {
+            if(temp.nextTimeAfter(from) < to && temp.nextTimeAfter(from) != -1)
+            {
+                actual.add(temp);
+            }
+        }
+        return actual;
+    }
 }
--- src/main/java/ua/edu/sumdu/j2se/KaplunDanil/tasks/Task.java
+++ src/main/java/ua/edu/sumdu/j2se/KaplunDanil/tasks/Task.java
@@ -1,2 +1,140 @@
-package ua.edu.sumdu.j2se.KaplunDanil.tasks;public class Task1 {
+package ua.edu.sumdu.j2se.KaplunDanil.tasks;
+
+public class Task {
+    private String title;
+    private int time;
+    private int start;
+    private int end;
+    private int interval;
+    private boolean active;
+    private boolean repeat;
+
+
+    public Task(String title, int time){
+        this.title = title;
+        this.time = time;
+        repeat = false;
+        active = false;
+    }
+
+    public Task(String title, int start, int end, int interval){
+        this.title = title;
+        time = start;
+        this.start = start;
+        this.end = end;
+        this.interval = interval;
+        repeat = true;
+        active= false;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isActive()
+    {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time)
+    {
+        this.time = time;
+        repeat = false;
+    }
+
+    public int getStartTime()
+    {
+        if(!repeat)
+        {
+            return time;
+        }
+        else
+        {
+            return start;
+        }
+    }
+
+    public int getEndTime()
+    {
+        if(!repeat)
+        {
+            return time;
+        }
+        else
+        {
+            return end;
+        }
+    }
+
+    public int getRepeatInterval()
+    {
+        if(!repeat)
+        {
+            return 0;
+        }
+        else
+        {
+            return interval;
+        }
+    }
+
+    public void setTime(int start, int end, int interval)
+    {
+        this.start = start;
+        this.end = end;
+        this.interval = interval;
+        this.time = start;
+        repeat = true;
+    }
+
+    public boolean isRepeated()
+    {
+        return repeat;
+    }
+
+    public int nextTimeAfter(int current){
+        if(!active)
+        {
+            return  -1;
+        }
+        else
+        {
+            if(!repeat)
+            {
+                if(time <= current)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return time;
+                }
+            }
+            else
+            {
+                int temp = start;
+                while (temp <= current)
+                {
+                    temp += interval;
+                    if(temp > end)
+                    {
+                        return -1;
+                    }
+                }
+                return temp;
+            }
+        }
+    }
 }
